4cbf3414129542d31e82ff5057f4a863f43712da
UlyPanda <ulypanda44@gmail.com>
2020-05-18 20:33:04 -0500

    Teamwork!!!

---
+++ todo.js
@@ -0,0 +1,58 @@
+ // We'll pre-populate this array with a couple objects just so it's not undefined if your internet connection isn't working properly.
+
+ let arrayOfTodos = [
+    {
+    "userId": 14,
+    "id": 1,
+    "title": "delectus aut autem",
+    "completed": false
+  },
+  {
+    "userId": 20,
+    "id": 2,
+    "title": "second delectus aut autem",
+    "completed": false
+  }]
+
+  const fetchTodos = () => {
+    fetch('https://jsonplaceholder.typicode.com/todos')
+    .then( (response) => response.json())
+    .then( (json) => arrayOfTodos = json)
+  }
+
+  const logTodos = () => {
+    console.log(arrayOfTodos)
+  }
+
+  const populateTodos = () => {
+
+    let todoList = document.getElementById('todo-list');
+
+
+ let todoTitle = "";
+ let todoComplete = "";
+
+    for (i=0; i < arrayOfTodos.length; i++) {
+        todoTitle = document.createTextNode(arrayOfTodos[0].title);
+      
+      //createElement to make a new LI
+    let todoItem = document.createElement("LI");
+     if (arrayOfTodos[i].completed) {
+       // add a class to the li to make text red
+       todoItem.classList.add("istrue")
+
+     } else {
+        todoItem.classList.add("isfalse")
+     }
+
+    todoItem.appendChild(todoTitle);
+
+    todoList.appendChild(todoItem);
+
+
+  } // end of for loop
+
+
+ console.log(arrayOfTodos[100].userId) 
+ console.log(arrayOfTodos[150].userId)
+} // end of function
